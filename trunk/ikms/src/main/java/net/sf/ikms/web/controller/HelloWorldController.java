e42433b5e09c057d303f513243fdcd2f178d7bd2
Oxidyc@gmail.com <Oxidyc@gmail.com@93697fa3-48ec-4134-3c87-18bfe64c5415>
2011-03-28 07:24:00 +0000

    spring 3.0.x MVC REST demo

---
+++ trunk/ikms/src/main/java/net/sf/ikms/web/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+/**
+ * @(#)HelloWorldController.java
+ * Copyright ikms.sourceforge.net 2003-2011
+ */
+package net.sf.ikms.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Spring 3.0.x MVC REST demo
+ * @author <b>oxidy</b>, Copyright &#169; 2003-2011
+ * @version 0.1,2011-3-28
+ */
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public ModelAndView helloWorld(){
+		String message = "Hello World, Welcome to Spring 3.0.x MVC REST!";
+		return new ModelAndView("hello","message",message);
+	}
+
+}
