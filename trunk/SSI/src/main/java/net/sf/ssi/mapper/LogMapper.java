0b790100d8d39ce04f7ea430877a790a4885aae2
oxidyc@gmail.com <oxidyc@gmail.com@93697fa3-48ec-4134-3c87-18bfe64c5415>
2011-07-01 16:12:32 +0000

    新增日志相关mapper

---
+++ trunk/SSI/src/main/java/net/sf/ssi/mapper/LogMapper.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)LogMapper.java
+ * Copyright (C) 2003-2011 ikms.sourceforge.net All rights reserved
+ */
+
+package net.sf.ssi.mapper;
+
+/**
+ * 日志
+ * 
+ * @author zhangyj
+ * @version 1.0
+ * @date 2011-7-1
+ * @param <T>
+ *
+ */
+public interface LogMapper<T> extends SqlMapper<T> {
+
+}
