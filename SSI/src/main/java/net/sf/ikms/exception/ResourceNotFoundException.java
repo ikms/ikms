4185b7c912d5e757c426e08b285a92fbfa14dbc8
oxidyc <Oxidyc@gmail.com>
2011-07-21 03:15:51 +0000

    新增资源未找到异常类

---
+++ SSI/src/main/java/net/sf/ikms/exception/ResourceNotFoundException.java
@@ -0,0 +1,31 @@
+/*
+ * @(#)ResourceNotFoundException.java
+ * Copyright (C) 2003-2011 ikms.sourceforge.net All rights reserved
+ */
+
+package net.sf.ikms.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 
+ * @author zhangyj
+ * @version 0.1, 2011-7-21 上午09:47:57
+ * 
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = -1699150784739814550L;
+	private Long resourceId;
+
+	public ResourceNotFoundException(Long resourceId) {
+		this.resourceId = resourceId;
+	}
+
+	public Long getResourceId() {
+		return resourceId;
+	}
+
+}
