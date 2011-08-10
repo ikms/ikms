0d5f5c0735eadb42ddca6a67133abd5e8b58944d
oxidyc@gmail.com <oxidyc@gmail.com@93697fa3-48ec-4134-3c87-18bfe64c5415>
2011-08-10 01:29:23 +0000

    新增对excel，json，rtf，zip文件的操作方法Java类，未实现其中方法

---
+++ trunk/utils/src/net/sf/ikms/util/file/ExcelUtils.java
@@ -0,0 +1,17 @@
+/*
+ * @(#)ExcelUtils.java
+ * Copyright (C) 2003-2011 ikms.sourceforge.net All rights reserved
+ */
+
+package net.sf.ikms.util.file;
+
+/**
+ * Excel文件相关操作方法工具类
+ * 
+ * @author <b>oxidy</b>, Copyright &#169; 2003
+ * @since 09 August 2011
+ * 
+ */
+public class ExcelUtils {
+
+}
---
+++ trunk/utils/src/net/sf/ikms/util/file/JSONUtils.java
@@ -0,0 +1,17 @@
+/*
+ * @(#)JSONUtils.java
+ * Copyright (C) 2003-2011 ikms.sourceforge.net All rights reserved
+ */
+
+package net.sf.ikms.util.file;
+
+/**
+ * JSON相关操作工具类
+ * 
+ * @author <b>oxidy</b>, Copyright &#169; 2003
+ * @since 09 August 2011
+ * 
+ */
+public class JSONUtils {
+
+}
