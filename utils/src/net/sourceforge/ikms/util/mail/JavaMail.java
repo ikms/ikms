9b4fb60df3f2c78fad58a98525f6779c5e19b6ec
oxidyc <Oxidyc@gmail.com>
2011-11-29 11:13:38 +0000

    新增发送邮件功能，和文件上传

---
+++ utils/src/net/sourceforge/ikms/util/mail/JavaMail.java
@@ -0,0 +1,14 @@
+/*
+ * @(#)JavaMail.java
+ * Copyright (C) 2003-2011 ikms.sourceforge.net All rights reserved 
+ */
+package net.sourceforge.ikms.util.mail;
+
+/**
+ * Java SDK 实现方式
+ * @author <b>oxidy</b>, Copyright &#169; 2003
+ * @version 1.0, 2011-11-23
+ */
+public class JavaMail {
+
+}
